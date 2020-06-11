9998e68e2c56d6206718eec2f11c8ffd895cd3a1
ivanBalev <43599235+ivanBalev@users.noreply.github.com>
2020-06-11 11:33:45 +0300

    Implemented SignIn with error handling

--- src/store/actions/authActions.js
+++ src/store/actions/authActions.js
@@ -1,5 +1,16 @@
 export const signIn = (credentials) => {
-    return(dispatch, getState, {getFirebase}) => {
-        
-    }    
+    return (dispatch, getState, { getFirebase }) => {
+        const firebase = getFirebase();
+
+        firebase.auth().signInWithEmailAndPassword(
+            credentials.email,
+            credentials.password
+        )
+            .then(() => {
+                dispatch({ type: 'LOGIN_SUCCESS' })
+            })
+            .catch((err) => {
+                dispatch({ type: 'LOGIN_ERROR', err })
+            })
+    }
 }
